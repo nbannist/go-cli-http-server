8d39fe59c087016611a3ee0915f7367f8ad10bd0
Nicholas Bannister-Andrews <nbannist@gmail.com>
2025-01-06 00:12:32 -0500

    initialized cobra/viper project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # go-cli-http-server
 An HTTP server written just to get a better understanding of HTTP
+
+Just a super simple HTTP server to better learn Go and also HTTP
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2025 Nicholas Bannister-Andrews
+
+*/
+package main
+
+import "github.com/nbannist/go-cli-http-server/cmd"
+
+func main() {
+	cmd.Execute()
+}
